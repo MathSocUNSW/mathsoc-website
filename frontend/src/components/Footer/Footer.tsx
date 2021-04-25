7022ecdcc685a1d899851ed13a9b96291586bb87
Alvin Cherk <alvin@cherk.org>
2021-04-25 21:22:36 +1000

    Convert footer to a grid

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -15,49 +15,53 @@ import {
 
 export default function Footer() {
   return (
-    <section className={styles.mainContainer}>
-      <div className={styles.leftText}>
-        <p>&#169; UNSW Mathematics Society 2020</p>
-      </div>
-      <div className={styles.rightList}>
-        <ul className={styles.logoList}>
-          <li>
-            <a href={facebookLink} target='_blank' rel='noreferrer'>
-              <div aria-label='facebook'>
-                <FacebookLogo className={styles.logo} />
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href={linkedInLink} target='_blank' rel='noreferrer'>
-              <div aria-label='linkedin'>
-                <LinkedinLogo className={styles.logo} />
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href={instagramLink} target='_blank' rel='noreferrer'>
-              <div aria-label='instagram'>
-                <InstagramLogo className={styles.logo} />
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href={youtubeLink} target='_blank' rel='noreferrer'>
-              <div aria-label='youtube'>
-                <YoutubeLogo className={styles.logo} />
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href={discordLink} target='_blank' rel='noreferrer'>
-              <div aria-label='youtube'>
-                <DiscordLogo className={styles.logo} />
-              </div>
-            </a>
-          </li>
-        </ul>
-      </div>
+    <section className={styles.footer}>
+      <section className={styles.footerLeft}></section>
+      <section className={styles.mainContainer}>
+        <div className={styles.leftText}>
+          <p>&#169; UNSW Mathematics Society 2020</p>
+        </div>
+        <div className={styles.rightList}>
+          <ul className={styles.logoList}>
+            <li>
+              <a href={facebookLink} target='_blank' rel='noreferrer'>
+                <div aria-label='facebook'>
+                  <FacebookLogo className={styles.logo} />
+                </div>
+              </a>
+            </li>
+            <li>
+              <a href={linkedInLink} target='_blank' rel='noreferrer'>
+                <div aria-label='linkedin'>
+                  <LinkedinLogo className={styles.logo} />
+                </div>
+              </a>
+            </li>
+            <li>
+              <a href={instagramLink} target='_blank' rel='noreferrer'>
+                <div aria-label='instagram'>
+                  <InstagramLogo className={styles.logo} />
+                </div>
+              </a>
+            </li>
+            <li>
+              <a href={youtubeLink} target='_blank' rel='noreferrer'>
+                <div aria-label='youtube'>
+                  <YoutubeLogo className={styles.logo} />
+                </div>
+              </a>
+            </li>
+            <li>
+              <a href={discordLink} target='_blank' rel='noreferrer'>
+                <div aria-label='youtube'>
+                  <DiscordLogo className={styles.logo} />
+                </div>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+      <section className={styles.footerRight}></section>
     </section>
   );
 }
