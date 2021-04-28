1ce3c0cd592bd61fae4eeaa12f2bbbaee36e388d
Alvin Cherk <alvin@cherk.org>
2021-04-29 02:06:52 +1000

    Card now uses props, and a few are shown on home

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,35 +1,44 @@
 import React from 'react';
 import style from 'src/styles/Card.module.scss';
 
-export default function Card() {
+type CardProps = {
+  imageLink: string;
+  eventName: string;
+  eventMainText: string;
+  eventDate?: string;
+  eventTime?: string;
+  eventLocation?: string;
+  externalLink: string;
+};
+
+export default function Card({
+  imageLink,
+  eventName,
+  eventMainText,
+  eventDate,
+  eventTime,
+  eventLocation,
+  externalLink
+}: CardProps): JSX.Element {
   return (
     <section className={style.mainContainer}>
       <section>
-        <img src='/images/events/tft.jpg' className={style.topImage} />
+        <img src={imageLink} className={style.topImage} alt='event banner' />
       </section>
       <section className={style.content}>
         <div>
-          <h1 className={style.title}>Event Name</h1>
+          <h1 className={style.title}>{eventName}</h1>
           <section className={style.eventInfo}>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a fermentum nisi,
-              blandit rhoncus nisl. Curabitur ligula odio, blandit vel elit in, finibus scelerisque
-              enim. Curabitur tincidunt tincidunt enim id suscipit. Maecenas odio quam, convallis
-              dignissim tincidunt eget, dapibus a lorem.
-            </p>
+            <p>{eventMainText}</p>
             <ul>
-              <li>Date: some date</li>
-              <li>Time: some time</li>
-              <li>Location: some location</li>
+              <li>Date: {eventDate}</li>
+              <li>Time: {eventTime}</li>
+              <li>Location: {eventLocation}</li>
             </ul>
           </section>
         </div>
         <section className={style.bottom}>
-          <a
-            href='https://www.youtube.com/channel/UC8FgY7QcGOrc_9b5qAoYfOg'
-            target='_blank'
-            rel='noreferrer'
-          >
+          <a href={externalLink} target='_blank' rel='noreferrer'>
             <p>Find out more &#8594;</p>
           </a>
         </section>
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Head from 'next/head';
 import Hero from '../components/Hero';
 import styles from 'src/styles/index.module.scss';
+import Card from 'src/components/Card';
 
 export default function index() {
   return (
@@ -20,7 +21,38 @@ export default function index() {
           resources for students. With over 3000 members, we are one of the largest societies at
           UNSW.
         </p>
-        <section className={styles.newEventsContainer}></section>
+        <section className={styles.newEventsContainer}>
+          <h1 className={styles.newEventsTitle}>Upcoming Events</h1>
+          <div className={styles.events}>
+            <Card
+              imageLink='/images/events/tft.jpg'
+              eventName='tft'
+              eventMainText='tft is cool'
+              eventDate='some date'
+              eventTime='some time'
+              eventLocation='some time'
+              externalLink='https://www.youtube.com/'
+            />
+            <Card
+              imageLink='/images/events/tft.jpg'
+              eventName='tft'
+              eventMainText='tft is cool'
+              eventDate='some date'
+              eventTime='some time'
+              eventLocation='some time'
+              externalLink='https://www.youtube.com/'
+            />
+            <Card
+              imageLink='/images/events/tft.jpg'
+              eventName='tft'
+              eventMainText='tft is cool'
+              eventDate='some date'
+              eventTime='some time'
+              eventLocation='some time'
+              externalLink='https://www.youtube.com/'
+            />
+          </div>
+        </section>
       </section>
     </section>
   );
