0d5ce44fae2d734f11894cb6ed937271121adea3
Alvin Cherk <alvin@cherk.org>
2021-04-29 01:19:00 +1000

    Move inline css to module.scss file

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 import Hero from '../components/Hero';
+import styles from 'src/styles/index.module.scss';
 
 export default function index() {
   return (
@@ -10,8 +11,8 @@ export default function index() {
         <meta name='keywords' content='mathsoc' />
       </Head>
       <Hero />
-      <section style={{ display: 'flex', justifyContent: 'center', paddingTop: '25px' }}>
-        <p style={{ textAlign: 'center', paddingLeft: '25%', paddingRight: '25%' }}>
+      <section className={styles.mainContainer}>
+        <p className={styles.descText}>
           UNSW Mathematics Society (MathSoc) is the constituent society of the UNSW School of
           Mathematics and Statistics, with membership open to anyone with an interest in
           mathematics. We support students by enhancing the sense of community amongst mathematics
@@ -19,6 +20,7 @@ export default function index() {
           resources for students. With over 3000 members, we are one of the largest societies at
           UNSW.
         </p>
+        <section className={styles.newEventsContainer}></section>
       </section>
     </section>
   );
