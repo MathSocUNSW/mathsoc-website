0b9679155702b37ccd3d343341de0fc990f4c320
Alvin Cherk <alvin@cherk.org>
2021-04-29 01:39:22 +1000

    Event card template

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,19 +1,39 @@
 import React from 'react';
-import style from '../styles/Card.module.scss';
-import Image from 'next/image';
+import style from 'src/styles/Card.module.scss';
 
 export default function Card() {
   return (
-    <div className={style.card}>
-      {/* Apparently these images are 1920 x 1080 px */}
-      <img src='/tft.jpg' />
-      <div className={style.content}>
-        <h1 className={style.title}>Event Name</h1>
-        <p className={style.description}>Description</p>
-        <a href='https://www.facebook.com' className={style.link}>
-          Find out more &rarr;
-        </a>
-      </div>
-    </div>
+    <section className={style.mainContainer}>
+      <section>
+        <img src='/images/events/tft.jpg' className={style.topImage} />
+      </section>
+      <section className={style.content}>
+        <div>
+          <h1 className={style.title}>Event Name</h1>
+          <section className={style.eventInfo}>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a fermentum nisi,
+              blandit rhoncus nisl. Curabitur ligula odio, blandit vel elit in, finibus scelerisque
+              enim. Curabitur tincidunt tincidunt enim id suscipit. Maecenas odio quam, convallis
+              dignissim tincidunt eget, dapibus a lorem.
+            </p>
+            <ul>
+              <li>Date: some date</li>
+              <li>Time: some time</li>
+              <li>Location: some location</li>
+            </ul>
+          </section>
+        </div>
+        <section className={style.bottom}>
+          <a
+            href='https://www.youtube.com/channel/UC8FgY7QcGOrc_9b5qAoYfOg'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <p>Find out more &#8594;</p>
+          </a>
+        </section>
+      </section>
+    </section>
   );
 }
