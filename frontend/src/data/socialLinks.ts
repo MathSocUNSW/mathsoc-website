9b4debcf84426d35169d31c4c25cc1667758b151
Alvin Cherk <alvin@cherk.org>
2021-05-06 21:36:55 +1000

    Update social links

--- frontend/src/data/socialLinks.ts
+++ frontend/src/data/socialLinks.ts
@@ -1,5 +1,5 @@
 export const facebookLink = 'https://www.facebook.com/unswmathsoc/';
 export const linkedInLink = 'https://au.linkedin.com/company/unsw-mathematics-society';
-export const youtubeLink = 'https://www.youtube.com/channel/UCV-y8pOXapCoo_DDBk2MpuA';
+export const youtubeLink = 'https://www.youtube.com/channel/UC8FgY7QcGOrc_9b5qAoYfOg';
 export const discordLink = 'https://discord.com/invite/Y7FFXxh';
-export const instagramLink = ''; // TODO
+export const instagramLink = 'https://instagram.com/unswmathsoc';
