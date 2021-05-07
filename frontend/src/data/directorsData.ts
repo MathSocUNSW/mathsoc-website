47e7e69560d43fa5222ded4d868bdae3b10e7016
Alvin Cherk <alvin@cherk.org>
2021-05-08 00:54:49 +1000

    Add exec/director data

---
+++ frontend/src/data/directorsData.ts
@@ -0,0 +1,38 @@
+type directorPerson = {
+  name: string;
+};
+
+type role = {
+  role:
+    | 'education'
+    | 'corporate events'
+    | 'social events'
+    | 'human resources'
+    | 'IT'
+    | 'marketing'
+    | 'sponsorships';
+  directors: Array<directorPerson>;
+};
+
+const directorsData: Array<role> = [
+  {
+    role: 'education',
+    directors: [{ name: 'John Kameas' }, { name: 'Raymond Li' }, { name: 'Gorden Zhuang' }]
+  },
+  { role: 'corporate events', directors: [{ name: 'Ting Li' }, { name: 'Andrew William' }] },
+  { role: 'social events', directors: [{ name: 'Vedant Rampal' }, { name: 'Daniel Kim' }] },
+  { role: 'human resources', directors: [{ name: 'Katherine Bai' }, { name: 'Christopher Wu' }] },
+  { role: 'IT', directors: [{ name: 'Andrew Xie' }] },
+  {
+    role: 'marketing',
+    directors: [
+      { name: 'Gowtham Ravikumar' },
+      { name: 'Celeste Thomson' },
+      { name: 'Jessica Zheng' },
+      { name: 'Jordan Shen' }
+    ]
+  },
+  { role: 'sponsorships', directors: [{ name: 'Abhi Khosla' }] }
+];
+
+export default directorsData;
---
+++ frontend/src/data/executivesData.ts
@@ -0,0 +1,14 @@
+type execPerson = {
+  name: string;
+  role: 'president' | 'vice-president' | 'secretary' | 'treasurer' | 'arc delegate';
+};
+
+const executivesData: Array<execPerson> = [
+  { name: 'Grace Li', role: 'president' },
+  { name: 'Philton Zhu', role: 'vice-president' },
+  { name: 'Justin Clarke', role: 'secretary' },
+  { name: 'Merry Chu', role: 'treasurer' },
+  { name: 'Gerald Huang', role: 'arc delegate' }
+];
+
+export default executivesData;
