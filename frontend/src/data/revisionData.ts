995ffbc7662e2eb036e7793a24529cb970cb63fb
Wellington Dulay <wellington.dulay@gmail.com>
2021-05-19 17:16:42 +1000

    started revision pg & Data structure

---
+++ frontend/src/data/revisionData.ts
@@ -0,0 +1,55 @@
+/*TODO: incomplete structure!*/
+export interface revisionTile {
+  title: string;
+  subTitle: string;
+  revisionLink: string;
+  iconPath: string;
+}
+
+/*TODO: real links & paths- real info*/
+const revisionData: revisionTile[] = [
+  {
+    title: "MATH1031",
+    subTitle: "Mathematics for Life Sciences (Labs)",
+    revisionLink: "---",
+    iconPath: "---"
+  },
+  {
+    title: "MATH1131/1141",
+    subTitle: "Mathematics 1A (Labs)",
+    revisionLink: "---",
+    iconPath: "---"
+  },
+  {
+    title: "MATH1131/1141",
+    subTitle: "Mathematics 1B (Labs)",
+    revisionLink: "---",
+    iconPath: "---"
+  },
+  {
+    title: "MATH1131/1141",
+    subTitle: "Mathematics 1A (Labs)",
+    revisionLink: "---",
+    iconPath: "---"
+  },
+  {
+    title: "MATH1031",
+    subTitle: "Mathematics for Life Sciences (Labs)",
+    revisionLink: "---",
+    iconPath: "---"
+  },
+  {
+    title: "MATH1131/1141",
+    subTitle: "Mathematics 1B (Labs)",
+    revisionLink: "---",
+    iconPath: "---"
+  },
+  {
+    title: "MATH1031",
+    subTitle: "Mathematics for Life Sciences (Labs)",
+    revisionLink: "---",
+    iconPath: "---"
+  },
+];
+
+export default revisionData;
--- frontend/src/pages/resources/revision.tsx
+++ frontend/src/pages/resources/revision.tsx
@@ -1,7 +1,42 @@
 import React from "react";
+import Head from "next/head";
+import { Container, Typography } from "@material-ui/core";
+
+import Hero from "src/components/Hero";
+
+{/*TODO: custom styles for resources*/}
+import styles from "src/styles/revision.module.scss";
 
 const Revision: React.FC = () => {
-  return <div>Revision</div>;
+  return (
+    <section>
+      <Head>
+        <title>MathSoc - Revision Lectures</title>
+        <meta name="keywords" content="mathsoc" />
+      </Head>
+      {/*TODO: custom hero for resources*/}
+      <Hero url="/images/resources/revisionBackground.png" text="Revision Lectures" />
+      <Container>
+          <section className={styles.content}>
+            <Typography variant="body1">
+            Here, you’ll find the slides and solutions for the revision seminars 
+            we held for UNSW Mathematics courses. All the best for your revision :)
+            <br/><br/>
+            Huge thanks to our Education team who worked tirelessly to bring you 
+            this material and present the revision seminars.
+            <br/><br/>
+            Please note that
+            <br/><br/>
+            Despite our best efforts in keeping our resources error-free, occasionally, 
+            one may slip past us. If you do happen to notice one, please do let us 
+            know ASAP by clicking here! These resources are NOT endorsed by the 
+            School of Mathematics and Statistics.
+            </Typography>
+          </section>
+        {/*TODO: 'tiles' for resources*/}
+      </Container>
+    </section>
+  );
 };
 
 export default Revision;
