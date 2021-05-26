fb51aeaa8edde5c996ddc8dcd78bd378031f48f9
Alvin Cherk <alvin@cherk.org>
2021-05-26 23:29:02 +1000

    Rename and seperating files

--- frontend/src/components/ResourcesHero.tsx
+++ frontend/src/components/ResourcesHero.tsx
@@ -1,36 +1,29 @@
 import React from "react";
 import { Typography } from "@material-ui/core";
-import styles from "src/styles/Hero.module.scss";
+import styles from "src/styles/ResourceHero.module.scss";
 
-interface HeroProps {
+interface ResourceHeroProps {
   url: string;
   text: string;
   icon: string;
   titlePosition: string;
 }
 
-const Hero: React.FC<HeroProps> = ({ 
-  url, 
-  text, 
-  icon, 
-  titlePosition
-}) => {
+const ResourceHero: React.FC<ResourceHeroProps> = ({ url, text, icon, titlePosition }) => {
   const titleFlip = titlePosition === "right";
 
   const image = {
-    backgroundImage: "url(" + url + ")",
+    backgroundImage: "url(" + url + ")"
   };
 
   return (
     <section>
       <Typography style={image} className={styles.hero} variant="h1" align="center">
-        <div className={titleFlip && styles.flip}>
-          {text}
-        </div>
+        <div className={titleFlip && styles.flip}>{text}</div>
         <img src={icon} className={styles.icon} alt="icon" draggable="false" />
       </Typography>
     </section>
   );
 };
 
-export default Hero;
+export default ResourceHero;
