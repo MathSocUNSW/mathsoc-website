da6651fc07a8102374c137247aaae6521a4fbef4
Alvin Cherk <alvin@cherk.org>
2021-05-27 00:01:58 +1000

    Template

--- frontend/src/pages/resources/exam-bank.tsx
+++ frontend/src/pages/resources/exam-bank.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import Head from "next/head";
+import { Container } from "@material-ui/core";
+
+import ResourceHero from "src/components/ResourceHero";
+import WholePageBox from "components/WholePageBox";
 
 const ExamBank: React.FC = () => {
-  return <div>exam</div>;
+  return (
+    <section>
+      <Head>
+        <title>MathSoc - Revision Lectures</title>
+        <meta name="keywords" content="mathsoc" />
+      </Head>
+      {/* <ResourceHero
+        url="/images/resources/revisionBackground.png"
+        text="Revision Lectures"
+        icon="/images/resources/revisionIcon.png"
+        titlePosition="right"
+      /> */}
+      <Container>
+        <WholePageBox>{"hello"}</WholePageBox>
+      </Container>
+    </section>
+  );
 };
 
 export default ExamBank;
