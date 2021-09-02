c6e7dc5d0d97df99ccaa09fed43c19486c0f64f3
Alvin Cherk <alvin@cherk.org>
2021-09-02 19:01:04 +1000

    Contact Us page

--- frontend/src/data/socialData.ts
+++ frontend/src/data/socialData.ts
@@ -2,36 +2,49 @@ export interface socialData {
   name: string;
   url: string;
   iconPath: string;
+  altName: string;
 }
 
 export const facebookData: socialData = {
   name: "Facebook",
   url: "https://www.facebook.com/unswmathsoc/",
-  iconPath: "/images/facebookLogo.svg"
+  iconPath: "/images/facebookLogo.svg",
+  altName: "UNSW Mathematics Society"
 };
 
 export const linkedInData: socialData = {
   name: "LinkedIn",
   url: "https://au.linkedin.com/company/unsw-mathematics-society/",
-  iconPath: "/images/linkedinLogo.svg"
+  iconPath: "/images/linkedinLogo.svg",
+  altName: "UNSW Mathematics Society"
 };
 
 export const instagramData: socialData = {
   name: "Instagram",
   url: "https://instagram.com/unswmathsoc",
-  iconPath: "/images/instagramLogo.svg"
+  iconPath: "/images/instagramLogo.svg",
+  altName: "unswmathsoc"
 };
 
 export const youtubeData: socialData = {
   name: "YouTube",
   url: "https://www.youtube.com/channel/UC8FgY7QcGOrc_9b5qAoYfOg/",
-  iconPath: "/images/youtubeLogo.svg"
+  iconPath: "/images/youtubeLogo.svg",
+  altName: "UNSW MathSoc Streams"
 };
 
 export const discordData: socialData = {
   name: "Discord",
   url: "https://discord.com/invite/Y7FFXxh/",
-  iconPath: "/images/discordLogo.svg"
+  iconPath: "/images/discordLogo.svg",
+  altName: "UNSW MathSoc"
+};
+
+export const gmailData: socialData = {
+  name: "Gmail",
+  url: "mailto:unswmathsoc@gmail.com",
+  iconPath: "/images/gmailLogo.svg",
+  altName: "unswmathsoc@gmail.com"
 };
 
 export const socials: socialData[] = [
@@ -41,6 +54,8 @@ export const socials: socialData[] = [
   youtubeData,
   discordData
 ];
+
+export const contactUsSocials: socialData[] = [...socials, gmailData];
 
 /*
 NOTES:
