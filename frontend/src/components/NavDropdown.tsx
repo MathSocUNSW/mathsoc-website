943ddfb27be8096fce9b704794941309d77bc672
Alvin Cherk <alvin@cherk.org>
2021-09-06 18:57:38 +1000

    Dropdown Nav bar

---
+++ frontend/src/components/NavDropdown.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Link from "next/link";
+
+import { subPage } from "src/data/navLinks";
+import styles from "src/styles/NavDropdown.module.scss";
+
+interface NavDropdownProps {
+  items: subPage[];
+  setDropdown: React.Dispatch<React.SetStateAction<boolean>>;
+  baseRoute: string;
+}
+
+const NavDropdown: React.FC<NavDropdownProps> = ({ items, setDropdown, baseRoute }) => {
+  return (
+    <div className={styles.mainContainer}>
+      {items.map((item) => (
+        <Link href={baseRoute + item.subRoute} key={item.name}>
+          <a>
+            <div className={styles.item} onClick={() => setDropdown(false)}>
+              {item.name}
+            </div>
+          </a>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default NavDropdown;
