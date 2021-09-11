75ba993972a35955fac3e1b8585b55851ca44461
Wellington Dulay <wellington.dulay@gmail.com>
2021-09-11 16:29:45 +1000

    Tile cleanup and animations

--- frontend/src/components/TileSlide.tsx
+++ frontend/src/components/TileSlide.tsx
@@ -1,21 +1,23 @@
 import React from "react";
 import { Typography } from "@material-ui/core";
-
-import TileLink from "./TileLink";
-import styles from "src/styles/tile.module.scss";
+import Link from "next/link";
 import { groups } from "src/data/revisionData";
 
-const TileGroup: React.FC<groups> = ({ groupHeader, groupLinks }) => {
+const TileSlide: React.FC<groups> = ({ groupHeader, groupLinks }) => {
   return (
     <div>
       <Typography variant="h5">{groupHeader}</Typography>
-      <ul className={styles.tileGroup}>
+      <ul>
         {groupLinks.map((link, index) => (
-          <TileLink {...link} key={index} />
+          <li key={index}>
+            <Link href={link.path}>
+              <a>{link.name}</a>
+            </Link>
+          </li>
         ))}
       </ul>
     </div>
   );
 };
 
-export default TileGroup;
+export default TileSlide;
