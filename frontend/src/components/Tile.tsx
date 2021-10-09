817d6dc3b2a2116341d5c65ed73ca2a3b55cf20b
Wellington Dulay <wellington.dulay@gmail.com>
2021-10-09 19:01:27 +1100

    resize margins & create tileVariables

--- frontend/src/components/Tile.tsx
+++ frontend/src/components/Tile.tsx
@@ -22,53 +22,55 @@ const Tile: React.FC<tileProps> = ({ courseCode, courseTitle, revisionLinks }) =
     // TODO: entire first page is clickable?
     <div className={styles.tile}>
       <div className={styles.tileContent}>
-        {/* Front Slide */}
-        {index < 0 && (
-          <div>
-            <img className={styles.icon} src="/images/resources/tileIcon.png" alt="icon" />
-            <Typography variant="h4" color="inherit">
-              {courseCode}
-            </Typography>
-            <Typography variant="h5" color="inherit">
-              {courseTitle}
-            </Typography>
-          </div>
-        )}
-        {/* Back Slide/s */}
-        {index >= 0 && (
-          <div>
-            <Typography variant="h4" color="inherit">
-              {courseCode}
-            </Typography>
-            <div>{slideVisibility}</div>
-          </div>
-        )}
-      </div>
+        <div className={styles.tileSlides}>
+          {/* Front Slide */}
+          {index < 0 && (
+            <div>
+              <img className={styles.icon} src="/images/resources/tileIcon.png" alt="icon" />
+              <Typography variant="h4" color="inherit">
+                {courseCode}
+              </Typography>
+              <Typography variant="h5" color="inherit">
+                {courseTitle}
+              </Typography>
+            </div>
+          )}
+          {/* Back Slide/s */}
+          {index >= 0 && (
+            <div>
+              <Typography variant="h4" color="inherit">
+                {courseCode}
+              </Typography>
+              <div>{slideVisibility}</div>
+            </div>
+          )}
+        </div>
 
-      {/* Navigation Bar */}
-      <div className={styles.nav}>
-        {/* Explore Arrow*/}
-        {index < 0 && (
-          <button onClick={() => setIndex(index + 1)} className={styles.exploreArrow}>
-            Explore
-            <img src="/images/resources/arrowRHS.png" alt="right-hand arrow" draggable="false" />
-          </button>
-        )}
-        {/* LHS Arrow */}
-        {index > -1 && (
-          <button onClick={() => setIndex(index - 1)} className={styles.arrow}>
-            <img src="/images/resources/arrowLHS.png" alt="left-hand arrow" draggable="false" />
-          </button>
-        )}
-        {/* RHS Arrow */}
-        {index > -1 && (
-          <button
-            onClick={() => setIndex(index + 1)}
-            className={index < revisionLinks.length - 1 ? styles.arrow : styles.disabled}
-          >
-            <img src="/images/resources/arrowRHS.png" alt="right-hand arrow" draggable="false" />
-          </button>
-        )}
+        {/* Navigation Bar */}
+        <div className={styles.nav}>
+          {/* Explore Arrow*/}
+          {index < 0 && (
+            <button onClick={() => setIndex(index + 1)} className={styles.exploreArrow}>
+              Explore
+              <img src="/images/resources/arrowRHS.png" alt="right-hand arrow" draggable="false" />
+            </button>
+          )}
+          {/* LHS Arrow */}
+          {index > -1 && (
+            <button onClick={() => setIndex(index - 1)} className={styles.arrow}>
+              <img src="/images/resources/arrowLHS.png" alt="left-hand arrow" draggable="false" />
+            </button>
+          )}
+          {/* RHS Arrow */}
+          {index > -1 && (
+            <button
+              onClick={() => setIndex(index + 1)}
+              className={index < revisionLinks.length - 1 ? styles.arrow : styles.disabled}
+            >
+              <img src="/images/resources/arrowRHS.png" alt="right-hand arrow" draggable="false" />
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
