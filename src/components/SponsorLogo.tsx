399fa4d4297d27a70e7b97fc2bb4abcd74df2fe9
Joanna Lin <linjoanna31@gmail.com>
2022-03-13 13:00:24 +1100

    Changed useRef and useEffect to useCallback to avoid rendering bugs

--- src/components/SponsorLogo.tsx
+++ src/components/SponsorLogo.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useCallback, useState } from "react";
 import styles from "../styles/SponsorsLogo.module.scss";
 
 const SQUARE_MIN_THRESHOLD = 5 / 6;
@@ -10,17 +10,18 @@ interface SponsorLogoProps {
   logoPath: string;
 }
 const SponsorLogo: React.FC<SponsorLogoProps> = ({ name, logoPath }) => {
-  const logoRef = useRef<HTMLImageElement>(null);
   const [logoClass, setLogoClass] = useState("");
-  useEffect(() => {
-    const image = logoRef.current as HTMLImageElement;
-    const aspectRatio = image.clientWidth / image.clientHeight;
-    if (SQUARE_MIN_THRESHOLD <= aspectRatio && aspectRatio <= SQUARE_MAX_THRESHOLD) {
-      setLogoClass(styles.logoSquare);
-    } else if (SQUARE_MAX_THRESHOLD < aspectRatio && aspectRatio <= WIDE_THRESHOLD) {
-      setLogoClass(styles.logoHorizontal);
-    } else {
-      setLogoClass(styles.logoLongHorizontal);
+  const logoRef = useCallback((logo: HTMLImageElement | null) => {
+    if (logo !== null) {
+      const logoImage = logo as HTMLImageElement;
+      const aspectRatio = logoImage.clientWidth / logoImage.clientHeight;
+      if (SQUARE_MIN_THRESHOLD <= aspectRatio && aspectRatio <= SQUARE_MAX_THRESHOLD) {
+        setLogoClass(styles.logoSquare);
+      } else if (SQUARE_MAX_THRESHOLD < aspectRatio && aspectRatio <= WIDE_THRESHOLD) {
+        setLogoClass(styles.logoHorizontal);
+      } else {
+        setLogoClass(styles.logoLongHorizontal);
+      }
     }
   }, []);
   return (
