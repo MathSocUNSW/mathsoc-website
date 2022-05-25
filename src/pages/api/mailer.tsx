01d09a891f3224eac4bd9bce1cb704a82c368b25
Anderson Pech <andersonpech@gmail.com>
2022-05-25 21:47:23 +1000

    adjusted error handling in mailer

--- src/pages/api/mailer.tsx
+++ src/pages/api/mailer.tsx
@@ -27,7 +27,7 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
     res.status(200).send("success");
   } catch (error: unknown) {
     if (error instanceof Error) {
-      res.status(500).send("Message could not be sent");
+      res.status(500).send(error.message);
     }
   }
 }
