3b0d1ed97e83d3536cef6aeb99401de65b45548f
John Wu <johnw0103@icloud.com>
2025-02-08 14:58:15 +1100

    Added hover effect for company logos

--- app/(main)/sponsors/page.tsx
+++ app/(main)/sponsors/page.tsx
@@ -27,43 +27,70 @@ const Sponsors: React.FC = async () => {
         </p>
       </section>
 
-
       {/* Sponsors */}
       <section className="py-6 px-6 mb-20">
         <h2 className="text-4xl text-center font-bold mb-20">Our Sponsors</h2>
         <div className="flex flex-col items-center space-y-8 mb-20">
-          <a href="https://www.janestreet.com/" target="_blank">
-            <img className="w-96" src="/images/logos/jane_street_logo.png" alt="jane street logo" />
+          <a href="https://www.janestreet.com/" target="_blank" rel="noopener noreferrer">
+            <img
+              className="w-96 transition-transform hover:scale-105"
+              src="/images/logos/jane_street_logo.png"
+              alt="jane street logo"
+            />
           </a>
         </div>
 
         <div className="flex flex-row justify-center items-center space-x-8 mb-20">
-          <a href="https://www.optiver.com/" target="_blank">
-            <img className="w-96" src="/images/logos/optiver_logo.png" alt="optiver logo" />
+          <a href="https://www.optiver.com/" target="_blank" rel="noopener noreferrer">
+            <img
+              className="w-96 transition-transform hover:scale-105"
+              src="/images/logos/optiver_logo.png"
+              alt="optiver logo"
+            />
           </a>
 
-          <a href="https://www.imc.com/" target="_blank">
-            <img className="w-96" src="/images/logos/imc_logo.png" alt="imc logo" />
+          <a href="https://www.imc.com/" target="_blank" rel="noopener noreferrer">
+            <img
+              className="w-96 transition-transform hover:scale-105"
+              src="/images/logos/imc_logo.png"
+              alt="imc logo"
+            />
           </a>
         </div>
 
         <div className="flex flex-row justify-center items-center space-x-8 mb-20">
-          <a href="https://www.quantium.com/" target="_blank">
-            <img className="w-96" src="/images/logos/quantium_logo.webp" alt="quantium logo" />
+          <a href="https://www.quantium.com/" target="_blank" rel="noopener noreferrer">
+            <img
+              className="w-96 transition-transform hover:scale-105"
+              src="/images/logos/quantium_logo.webp"
+              alt="quantium logo"
+            />
           </a>
 
-          <a href="https://www.citadelsecurities.com/" target="_blank">
-            <img className="w-96" src="/images/logos/citadel_securities_logo.webp" alt="citadel logo" />
+          <a href="https://www.citadelsecurities.com/" target="_blank" rel="noopener noreferrer">
+            <img
+              className="w-96 transition-transform hover:scale-105"
+              src="/images/logos/citadel_securities_logo.webp"
+              alt="citadel logo"
+            />
           </a>
         </div>
 
         <div className="flex flex-row justify-center items-center space-x-8 mb-20">
-          <a href="https://www.flowtraders.com/" target="_blank">
-            <img className="w-96" src="/images/logos/flow_traders_logo.webp" alt="flow traders logo" />
+          <a href="https://www.flowtraders.com/" target="_blank" rel="noopener noreferrer">
+            <img
+              className="w-96 transition-transform hover:scale-105"
+              src="/images/logos/flow_traders_logo.webp"
+              alt="flow traders logo"
+            />
           </a>
-          
-          <a href="https://www.auroraenergy.com.au/" target="_blank">
-            <img className="w-96" src="/images/logos/aurora_logo.webp" alt="aurora logo" />
+
+          <a href="https://www.auroraenergy.com.au/" target="_blank" rel="noopener noreferrer">
+            <img
+              className="w-96 transition-transform hover:scale-105"
+              src="/images/logos/aurora_logo.webp"
+              alt="aurora logo"
+            />
           </a>
         </div>
       </section>
