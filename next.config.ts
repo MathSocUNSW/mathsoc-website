7dc181e20cb0de41cb6a1a5f7edcaa72f5f6b7b6
eric <eric@ericVM.myguest.virtualbox.org>
2025-02-10 03:30:41 +1100

    fixing event images

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,11 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "scontent.fsyd14-1.fna.fbcdn.net",
+        hostname: "*.fbcdn.net", // Allow all Facebook CDN subdomains
+      },
+      {
+        protocol: "https",
+        hostname: "platform-lookaside.fbsbx.com", // Facebook profile images
       },
     ],
   },
