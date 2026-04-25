079b267efd930a7a89ddd8ae1173d5038fc3f5e0
Eric Wong <z5692161@ad.unsw.edu.au>
2026-04-25 19:24:16 +1000

    UPDATE: New pictures

--- app/(main)/about-us/page.tsx
+++ app/(main)/about-us/page.tsx
@@ -92,7 +92,7 @@ const About: React.FC = () => {
           { name: "Jasmine Chong-White", role: "Treasurer & Arc Delegate", photo: "none.jpg" },
           { name: "Lori Ieong", role: "Vice President of Activities", photo: "none.jpg" },
           { name: "Archit Aggarwal", role: "Vice President of Development", photo: "none.jpg" },
-          { name: "Yufan Han", role: "Vice President of Education", photo: "none.jpg" },
+          { name: "Yufan Han", role: "Vice President of Education", photo: "yufan.jpg" },
           { name: "Justin Cao", role: "Vice President of Operations", photo: "none.jpg" },
         ],
       },
@@ -126,7 +126,7 @@ const About: React.FC = () => {
           { name: "Mina Woo", role: "Socials Director", photo: "none.jpg" },
           { name: "Marcel Hambali", role: "Socials Director", photo: "none.jpg" }
         ],
-        subcommittee: ["Janindu Wijayagunawardana", "Alice Li", "Stephy Wu", "Anthony Law", "Annice Ip", "Kevin Lee"],
+        subcommittee: ["Janindu W", "Alice Li", "Stephy Wu", "Anthony Law", "Annice Ip", "Kevin Lee"],
       },
       "Human Resources": {
         directors: [
@@ -154,7 +154,7 @@ const About: React.FC = () => {
       "Information Technology": {
         directors: [
           { name: "Regan Benedetti", role: "IT Director", photo: "/images/headshots/regan.jpg" },
-          { name: "Khang Nguyen", role: "IT Director", photo: "none.jpg" }
+          { name: "Khang Nguyen", role: "IT Director", photo: "khang.jpg" }
         ],
         subcommittee: ["Eric Wong", "Xiayan Ma", "Cheng Hao Li", "Jasmine Hui", "Stella Zhou", "Christine Chen"],
       },
